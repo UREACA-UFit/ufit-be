9d718bb22a5718bc3b448f19b583463f1574fa82
dudxo <father8475@naver.com>
2025-06-10 00:47:35 +0900

    feat: auditing configuration 추가

--- src/main/java/com/ureca/ufit/global/config/MongoConfig.java
+++ src/main/java/com/ureca/ufit/global/config/MongoConfig.java
@@ -1,9 +1,27 @@
 package com.ureca.ufit.global.config;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+import com.ureca.ufit.global.util.DateToLocalDateTimeKstConverter;
+import com.ureca.ufit.global.util.LocalDateTimeToDateKstConverter;
 
 @Configuration
 @EnableMongoAuditing
 public class MongoConfig {
+
+	@Bean
+	public MongoCustomConversions customConversions(
+		LocalDateTimeToDateKstConverter localDateTimeToDateKstConverter,
+		DateToLocalDateTimeKstConverter dateToLocalDateTimeKstConverter) {
+
+		return new MongoCustomConversions(Arrays.asList(
+			localDateTimeToDateKstConverter,
+			dateToLocalDateTimeKstConverter
+		));
+	}
 }
