695c87b7057b997298889f9c6b394e4d571e019e
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-06-10 08:32:44 +0900

    test: login, logout, reissue 단위 테스트

--- src/test/java/com/ureca/ufit/common/fixture/UserFixture.java
+++ src/test/java/com/ureca/ufit/common/fixture/UserFixture.java
@@ -2,13 +2,15 @@ package com.ureca.ufit.common.fixture;
 
 import com.ureca.ufit.entity.User;
 
+import com.ureca.ufit.entity.enums.Gender;
+import com.ureca.ufit.entity.enums.Role;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserFixture {
 
-	public static User user() {
-		return null;
+	public static User createUser(String username, String password, int age, int famliy, Gender gender, Role role, String ratePlanId) {
+		return User.of(username, password, age, famliy, gender, role, ratePlanId);
 	}
 }
