bbab10ea09e4c15a492081547260308edc6870c0
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-06-10 16:55:18 +0900

    feat: user mapper 추가

--- src/main/java/com/ureca/ufit/domain/user/service/UserService.java
+++ src/main/java/com/ureca/ufit/domain/user/service/UserService.java
@@ -1,8 +1,8 @@
 package com.ureca.ufit.domain.user.service;
 
+import com.ureca.ufit.domain.user.dto.UserMapper;
 import com.ureca.ufit.domain.user.dto.request.RegisterRequest;
 import com.ureca.ufit.domain.user.dto.response.RegisterResponse;
-import com.ureca.ufit.entity.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,21 +18,11 @@ public class UserService {
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
 
-
 	//DB 확인용 회원가입
 	@Transactional
 	public RegisterResponse register(RegisterRequest request) {
 		String encodedPassword = passwordEncoder.encode(request.password());
-
-		User user = User.of(request.email(),
-				encodedPassword,
-				request.age(),
-				request.family(),
-				request.gender(),
-				request.role(),
-				request.ratePlanId());
-		userRepository.save(user);
-
+		userRepository.save(UserMapper.toEntity(request, encodedPassword));
 		return new RegisterResponse(true);
 	}
 
