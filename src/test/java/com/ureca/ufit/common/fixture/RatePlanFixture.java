b09caedd4dc57fa8e77899a17e7abdee3cf18fa8
dudxo <father8475@naver.com>
2025-06-10 23:30:12 +0900

    test: RatePlan Fixture 추가

---
+++ src/test/java/com/ureca/ufit/common/fixture/RatePlanFixture.java
@@ -0,0 +1,28 @@
+package com.ureca.ufit.common.fixture;
+
+import static lombok.AccessLevel.*;
+
+import java.util.Map;
+
+import com.ureca.ufit.entity.RatePlan;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public class RatePlanFixture {
+
+	public static RatePlan ratePlan(String planName, int monthlyFee) {
+		return RatePlan.of(
+			planName,
+			"요금제",
+			monthlyFee,
+			1000,
+			"100G",
+			"무제한",
+			"무제한",
+			Map.of("benefit", "basic"),
+			true,
+			true
+		);
+	}
+}
