c303e8386c039ccc09ef1814914b90b6511a7612
Gain Lee <55633213+LGAIN@users.noreply.github.com>
2025-06-12 17:23:54 +0900

    fix: 충돌 해결

--- src/test/java/com/ureca/ufit/common/fixture/ChatBotReviewFixture.java
+++ src/test/java/com/ureca/ufit/common/fixture/ChatBotReviewFixture.java
@@ -10,25 +10,15 @@ import static lombok.AccessLevel.PRIVATE;
 @NoArgsConstructor(access = PRIVATE)
 public class ChatBotReviewFixture {
 
-    public static ChatBotReview chatBotReview(int rating, Map<String, Object> recommendPlan) {
-        return ChatBotReview.of(
-                "추천 퀄리티가 너무 좋아서…",
-                rating,
-                recommendPlan,
-                "넉넉한 데이터로 마음놓고 사용가능한 요금제");
-    }
+	public static ChatBotReview chatBotReview(int rating, Map<String, Object> recommendPlan) {
+		return ChatBotReview.of(
+			"추천 퀄리티가 너무 좋아서…",
+			rating,
+			recommendPlan,
+			"넉넉한 데이터로 마음놓고 사용가능한 요금제");
+	}
 
-    public static ChatBotReview chatBotReview(
-            int rating,
-            Map<String, Object> recommendPlan   
-    ) {
-        ChatBotReview review = ChatBotReview.of(
-                "추천 퀄리티가 너무 좋아서…",
-                rating,
-                recommendPlan,
-                "넉넉한 데이터로 마음놓고 사용가능한 요금제");
-        return review;
-    }
+
 
 	public static ChatBotReview chatBotReview(String content, String questionSummary) {
 		return ChatBotReview.of(
