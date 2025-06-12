81c5b5f4b234fea05e3badb214065859e5b3d490
Eric-HAN-01 <hhw3422@gmail.com>
2025-06-12 21:40:04 +0900

    feat: 요금제 목록/상세 조회 기능 추가

--- src/main/java/com/ureca/ufit/domain/rateplan/repository/RatePlanQueryRepository.java
+++ src/main/java/com/ureca/ufit/domain/rateplan/repository/RatePlanQueryRepository.java
@@ -1,9 +1,22 @@
 package com.ureca.ufit.domain.rateplan.repository;
 
 import com.ureca.ufit.domain.admin.dto.response.AdminRatePlanResponse;
+import com.ureca.ufit.domain.rateplan.dto.response.RatePlanDetailResponse;
+import com.ureca.ufit.domain.rateplan.dto.response.RatePlanPreviewResponse;
 import com.ureca.ufit.global.dto.CursorPageResponse;
+import com.ureca.ufit.entity.RatePlan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 public interface RatePlanQueryRepository {
 
 	public CursorPageResponse<AdminRatePlanResponse> getRatePlansByCursor(String cursor, int size, String type);
+
+	Page<RatePlan> findEnabledRatePlansWithSort(Pageable pageable, String sortType);
+
+	Page<RatePlanPreviewResponse> getRatePlanPreviews(Pageable pageable, String sortType);
+
+	Optional<RatePlanDetailResponse> getRatePlanDetailById(String id);
 }
