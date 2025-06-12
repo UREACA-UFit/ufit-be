dc770102db359c9778070529e632c37a84ee9b72
Gain Lee <heli1420@naver.com>
2025-06-12 16:41:03 +0900

    fix: git 오류 해결

---
+++ src/main/java/com/ureca/ufit/domain/admin/dto/ChatBotReviewMapper.java
@@ -0,0 +1,34 @@
+package com.ureca.ufit.domain.admin.dto;
+
+import static lombok.AccessLevel.*;
+
+import com.ureca.ufit.domain.chatbot.dto.request.CreateChatBotReviewRequest;
+import com.ureca.ufit.domain.chatbot.dto.response.CreateChatBotReviewResponse;
+import com.ureca.ufit.domain.chatbot.dto.response.QuestionSummaryDto;
+import com.ureca.ufit.entity.ChatBotReview;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public class ChatBotReviewMapper {
+
+	private static final String CHAT_REVIEW_SUCCESS_MSG = "리뷰가 정상적으로 제출되었습니다.";
+
+	//    private static ChatBotReviewResponse toChatBotReview(){
+	//        return ;
+	//    }
+
+	public static CreateChatBotReviewResponse toCreateChatBotReviewResponse() {
+		return new CreateChatBotReviewResponse(CHAT_REVIEW_SUCCESS_MSG);
+	}
+
+	public static ChatBotReview toChatBotReview(CreateChatBotReviewRequest request,
+		QuestionSummaryDto questionSummaryDto) {
+		return ChatBotReview.of(
+			request.content(),
+			request.rating(),
+			request.recommendPlans(),
+			questionSummaryDto.questionSummary()
+		);
+	}
+}
