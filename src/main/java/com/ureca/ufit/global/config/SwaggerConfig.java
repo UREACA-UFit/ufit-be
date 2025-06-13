88a62e94848be5ce25ea7457ddb15de8d3185cd4
dudxo <father8475@naver.com>
2025-06-13 09:00:08 +0900

    feat: swagger https(develop)/http(local) server 추가

--- src/main/java/com/ureca/ufit/global/config/SwaggerConfig.java
+++ src/main/java/com/ureca/ufit/global/config/SwaggerConfig.java
@@ -4,12 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
+@OpenAPIDefinition(
+	info = @io.swagger.v3.oas.annotations.info.Info(
+		title = "UFit API API",
+		description = "UFit API 명세서",
+		version = "v.1.0"),
+	servers = {
+		@Server(url = "https://be.u-fit.site", description = "Deploy Server URL"),
+		@Server(url = "http://localhost:8080", description = "Local Host URL")}
+)
 @Configuration
 public class SwaggerConfig {
 
